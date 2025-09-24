16642261dde6cab0483395845a55a45ce3e63bab
yosephcortez698-pixel <yosephcortez698@gmail.com>
2025-09-24 15:30:40 -0500

    12

--- Animal.java
+++ Animal.java
@@ -1,4 +1,25 @@
 public class Animal {
 
-    public void emitirSonido() {
-}
+    protected String nombre;
+    protected int edad;
+    protected String dueño;
+    protected String color;
+    protected String raza;
+    protected String Tipo;
+
+    public Animal(String nombre, int edad, String dueño, String color, String raza, String Tipo) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.dueño = dueño;
+        this.color = color;
+        this.raza = raza;
+        this.Tipo = Tipo;
+    }
+
+    public void haserSonido() {
+        System.out.println("Haser sonido");
+    }
+
+    public String Setnombre(){
+        get
+    }
--- perro.java
+++ perro.java
@@ -1,6 +1,18 @@
-public class perro {
+public class perro extends Animal {
 
-    public void perro(String nombre, int edad, int peso, String color, String raza) {
+    private String nombre;
+    private int edad;
+    private String dueño;
+    private String color;
+    private String raza;
+    private String Tipo;
 
+    public void Perro(String nombre, int edad, String dueño, String color, String raza, String Tipo) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.dueño = dueño;
+        this.color = color;
+        this.raza = raza;
+        this.Tipo = Tipo;
     }
 }
